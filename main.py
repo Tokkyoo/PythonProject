ea10ccb27e8da048d07464083d30c9ffa8ece1c9
Tokkyoo <71498574+Tokkyoo@users.noreply.github.com>
2024-03-12 15:43:37 +0100

    Ajout d'un template pour avoir la liste des animés vus, ajout méthode de login temporaire avec admin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,7 +7,36 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+def valid_login(username, password):
+    # Cette fonction devrait vérifier si le nom d'utilisateur et le mot de passe sont valides.
+    # Vous pouvez implémenter la validation selon vos besoins.
+    return username == 'admin' and password == 'password'
 
-@app.route("/login")
+
+def log_the_user_in(username):
+    # Cette fonction pourrait enregistrer l'utilisateur connecté dans une session ou effectuer d'autres actions.
+    return f'Logged in as {username}'
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template("login.html")
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+@app.route('/')
+def index():
+    username = request.cookies.get('username')
+    # use cookies.get(key) instead of cookies[key] to not get a
+    # KeyError if the cookie is missing.
+
+
+@app.route("/animelist")
+def animelist():
+    return render_template('animelist.html')
