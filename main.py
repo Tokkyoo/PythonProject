95a9d444a8979cac96090e23349f46c0871d01be
Tokkyoo <71498574+Tokkyoo@users.noreply.github.com>
2024-03-12 14:54:14 +0100

    Ajout d'une page login et du route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,3 +6,8 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
